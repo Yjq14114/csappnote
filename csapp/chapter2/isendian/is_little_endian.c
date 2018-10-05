662306b80280bbc04ae5a05e31af6a64b2409643
yjq <yjq14114@gmail.com>
2018-10-05 23:19:58 +0800

    判断小端机器

--- csapp/chapter2/isendian/is_little_endian.c
+++ csapp/chapter2/isendian/is_little_endian.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+int is_little_endian()
+{
+    int x = 1;
+    printf("%x\n", x);
+    char y = (char)x;
+    return y;
+}
 void main(int argc, char **argv[])
 {
     int a = is_little_endian();
     printf("%d\n", a);
-}
-int is_little_endian()
-{
-    int x = 1;
-    char y = (char)x;
-    return y;
 }
