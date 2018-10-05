261a31dc37a4ca9e72b028f41618f47195c798db
yjq <yjq14114@gmail.com>
2018-10-05 13:33:52 +0800

    c

--- binary/binary.c
+++ binary/binary.c
@@ -2,14 +2,12 @@
 void to_binary(unsigned int n);
 void  main(int argc, char ** argv[]) 
 {
-	double a = 10.0;
-	printf("%l20.0\n");
 	unsigned int n = 5;
 	to_binary(5);
-	// for(int i = 0; i < 5; i++) 
-	// {
-	// 	printf("\n%c", i + 0x30);
-	// }
+	for(int i = 0; i < 5; i++) 
+	{
+		printf("\n%c", i + 0x30);
+	}
 }
 
 void to_binary(unsigned int n) {
