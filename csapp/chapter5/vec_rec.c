fca203921019d63d6d81c720af236d6c6ade5f14
yangjiaqi <yang.jiaqi@vpclub.cn>
2019-05-18 19:43:17 +0800

    消除低效率循环

--- csapp/chapter5/vec_rec.c
+++ csapp/chapter5/vec_rec.c
@@ -80,3 +80,15 @@ void combine3(vec_ptr v, data_t * dest) {
         *dest = *dest OP data[i];
     }
 }
+
+/* Accumulate result in local variable*/
+void combine4(vec_ptr v, data_t *dest) {
+    long i;
+    long length = vec_length(v);
+    data_t *data = get_vec_start(v);
+    data_t acc = IDENT;
+    for(i = 0; i < length; i++) {
+        acc = acc OP data[i];
+    }
+    *dest = acc;
+}
