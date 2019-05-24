1878ceeffcf98bb393f496bb3db3aba043a29efa
yangjiaqi <yang.jiaqi@vpclub.cn>
2019-05-24 13:52:50 +0800

    循环展开

--- csapp/chapter5/vec_rec.c
+++ csapp/chapter5/vec_rec.c
@@ -92,3 +92,20 @@ void combine4(vec_ptr v, data_t *dest) {
     }
     *dest = acc;
 }
+void combine5(vec_ptr v, data_t *dest) {
+    long i;
+    long length = vec_length(v);
+    long limit = length - 1;
+    data_t *data = get_vec_start(v);
+    data_t acc = IDENT;
+
+    for ( i = 0; i < limit; i+=2)
+    {
+        /* code */
+        acc = (acc OP data[i]) OP data[i+1];
+    }
+    for (; i < length; i++) {
+        acc = acc OP data[i];
+    }
+    *dest = acc;
+}
