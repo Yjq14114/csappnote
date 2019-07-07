679b64234c8c4e0e4caa6b08bd2970db2f2eec64
yangjiaqi <yang.jiaqi@vpclub.cn>
2019-07-07 20:14:59 +0800

    进程图

--- csapp/chapter8/callError.c
+++ csapp/chapter8/callError.c
@@ -8,6 +8,10 @@ void unix_error(char *msg)
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
 }
+/**
+ * 打印异常
+ */
+
 pid_t Fork(void)
 {
     pid_t pid;
@@ -17,4 +21,26 @@ pid_t Fork(void)
         /* code */
     }
     return pid;
+}
+/**
+ * @note fork函数的特点
+ * 1.调用一次,返回两次
+ * 2.并发执行
+ * 3.相同但是独立的地址空间
+ * 4.共享文件
+ */
+int main()
+{
+    pid_t pid;
+    int x = 1;
+    pid = Fork();
+    if (pid == 0)
+    {
+        /* child */
+        printf("child : x=%d\n", ++x);
+        exit(0);
+    }
+    /*parent */
+    printf("parent: x=%d\n", --x);
+    exit(0);
 }
